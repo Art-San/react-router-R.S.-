b4d3c7d355cfd657b23211140813e61ae52c988e
Aleksander <asava@sibmail.com>
2022-08-22 16:38:24 +0700

    end

--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -1,5 +1,28 @@
-const Posts = () => {
-    return ( <h1>Posts</h1> );
+import PostsList from "./postsList";
+import Post from "./post";
+import { useParams } from "react-router-dom";
+
+const Posts = ({ match }) => {
+    const params = useParams()
+    const posts = [
+        {id: 1, label: "post 1"},
+        {id: 2, label: "post 2"},
+        {id: 3, label: "post 3"}
+      ];
+ 
+      const { postId } = params;
+    
+  
+    return (
+        <>
+            {postId ? (
+                <Post posts={posts} id={postId} />
+            
+                ) : (
+                <PostsList posts={posts} />
+                )}
+            </>
+        );
 }
  
 export default Posts;
